cc8d6fa33dde8d72b9079f7d92e5f15ad45e58c4
Gandinya <muhammaddedegandi01@gmail.com>
2025-07-24 09:16:55 +0700

    Update data.js

--- Database/data.js
+++ Database/data.js
@@ -1,4 +1,7 @@
-let produkList = [
+const STORAGE_KEY = "produkList";
+
+// Ambil data dari localStorage, jika belum ada gunakan data default
+let produkList = JSON.parse(localStorage.getItem(STORAGE_KEY)) || [
   {
     nama: "Celana Baggy wanita",
     harga: 146850,
@@ -14,20 +17,32 @@ let produkList = [
     harga: 68400,
     video: "KemejaCeweFitBody.mp4"
   }
-]
+];
 
+// Simpan ke localStorage
+function simpanKeStorage() {
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(produkList));
+}
+
+// Ambil seluruh data produk
 function getProduk() {
-  return produkList
+  return produkList;
 }
 
+// Tambah produk baru
 function tambahProduk(produk) {
-  produkList.push(produk)
+  produkList.push(produk);
+  simpanKeStorage();
 }
 
+// Perbarui produk berdasarkan index
 function updateProduk(index, produkBaru) {
-  produkList[index] = produkBaru
+  produkList[index] = produkBaru;
+  simpanKeStorage();
 }
 
+// Hapus produk
 function hapusProduk(index) {
-  produkList.splice(index, 1)
+  produkList.splice(index, 1);
+  simpanKeStorage();
 }
