a08b1f3cbdbe4b5ac85b47926f865f282516465b
Gandinya <muhammaddedegandi01@gmail.com>
2025-07-24 16:20:56 +0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -7,14 +7,14 @@ function renderProduk() {
     const card = document.createElement("div")
     card.className = "bg-white rounded shadow p-4"
     card.innerHTML = `
-       <video class="w-full h-auto rounded-t-xl" autoplay muted loop playsinline>
+      <video controls class="w-full rounded mb-2">
         <source src="video/${item.video}" type="video/mp4" />
         Browser tidak mendukung video.
       </video>
       <h3 class="text-lg font-bold">${item.nama}</h3>
       <p class="text-orange-600 font-semibold">Rp${item.harga.toLocaleString()}</p>
       <div class="mt-2 flex gap-2">
-       <a href="/CRUD/edit.html?index=${i}" class="bg-yellow-400 px-3 py-1 rounded text-sm">Edit</a>
+        <a href="edit.html?index=${i}" class="bg-yellow-400 px-3 py-1 rounded text-sm">Edit</a>
         <button onclick="hapusProdukHandler(${i})" class="bg-red-500 text-white px-3 py-1 rounded text-sm">Hapus</button>
       </div>
     `
