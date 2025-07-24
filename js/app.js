3c51bed3ff53bfc5c1afbe963ec2810d7fa9e844
Gandinya <muhammaddedegandi01@gmail.com>
2025-07-24 09:40:39 +0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -14,7 +14,10 @@ function renderProduk() {
       <h3 class="text-lg font-bold">${item.nama}</h3>
       <p class="text-orange-600 font-semibold">Rp${item.harga.toLocaleString()}</p>
       <div class="mt-2 flex gap-2">
-        <a href="/CRUD/edit.html?index=${i}" class="bg-yellow-400 px-3 py-1 rounded text-sm">Edit</a>
+        <a href="/CRUD/edit.html?index=${i}" class="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-cyan-500 to-blue-500 group-hover:from-cyan-500 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-cyan-200 dark:focus:ring-cyan-800">
+        <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-transparent group-hover:dark:bg-transparent">
+        Edit
+        </a>
         <button onclick="hapusProdukHandler(${i})" class="bg-red-500 text-white px-3 py-1 rounded text-sm">Hapus</button>
       </div>
     `
