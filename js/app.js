66cd1bacd1ddd46fae67aac82f3775f707e96221
Gandinya <muhammaddedegandi01@gmail.com>
2025-07-24 09:41:52 +0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -18,7 +18,10 @@ function renderProduk() {
         <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-transparent group-hover:dark:bg-transparent">
         Edit
         </a>
-        <button onclick="hapusProdukHandler(${i})" class="bg-red-500 text-white px-3 py-1 rounded text-sm">Hapus</button>
+        <button onclick="hapusProdukHandler(${i})" class="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-cyan-500 to-blue-500 group-hover:from-cyan-500 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-cyan-200 dark:focus:ring-cyan-800">
+        <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-transparent group-hover:dark:bg-transparent">
+        Hapus
+        </button>
       </div>
     `
     container.appendChild(card)
