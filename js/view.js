e7ed97752a17bccbab63e530bc9761ecbc17a653
Gandinya <muhammaddedegandi01@gmail.com>
2025-07-25 16:36:54 +0700

    Create view.js

---
+++ js/view.js
@@ -0,0 +1,12 @@
+<a href="https://s.shopee.co.id/9zmy31XSGD" target="_blank" class="product block group overflow-hidden rounded-xl shadow-lg bg-white" data-name="celana cargo baggy wanita">
+        <video class="w-full h-auto rounded-t-xl" autoplay muted loop playsinline>
+          <source src="/video/OUTFIT BAGGY.mp4" type="video/mp4" />
+        </video>
+        <div class="p-3 text-center">
+          <p class="font-medium text-gray-700">Celana Cargo Baggy Wanita</p>
+          <div class="flex justify-center items-center gap-2 mt-1 text-gray-600">
+            <i class="fa-solid fa-tags text-orange-500"></i>
+            <span class="text-base font-medium">Rp146.850</span>
+          </div>
+        </div>
+      </a>
