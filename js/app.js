423dcf43fd178a1ce515a1f348501cef8ce8a291
Gandinya <muhammaddedegandi01@gmail.com>
2025-07-28 11:39:18 +0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -5,6 +5,59 @@ const STORAGE_KEY = "produkListZarg"
 
 let produkList = loadProdukList()
 
+let produkList = loadProdukList()
+
+if (produkList.length === 0) {
+  const dataAwal = [
+    {
+      nama: "Celana Cargo Baggy Wanita",
+      harga: 146850,
+      video: "OUTFIT BAGGY.mp4",
+      link: "https://s.shopee.co.id/9zmy31XSGD",
+      dihapus: false
+    },
+    {
+      nama: "Workshirt Cutting Boxy Pria",
+      harga: 197500,
+      video: "Workshirt Boxy.mp4",
+      link: "https://s.shopee.co.id/4ApBKfHkUy",
+      dihapus: false
+    },
+    {
+      nama: "Kemeja Fit Body Wanita",
+      harga: 68400,
+      video: "KemejaCeweFitBody.mp4",
+      link: "https://s.shopee.co.id/8ALKJ88hGX",
+      dihapus: false
+    },
+    {
+      nama: "Hangover - WOLLARD - Kaos Lengan Pendek Pria & Wanita",
+      harga: 75000,
+      video: "Hangover - WOLLARD.mp4",
+      link: "https://s.shopee.co.id/8pb8qWE95N",
+      dihapus: false
+    },
+    {
+      nama: "Hangover - HGVR 2.0 - Kaos Lengan Pendek Pria & Wanita",
+      harga: 75000,
+      video: "HGVR 2.0.mp4",
+      link: "https://s.shopee.co.id/8KesFklkU5",
+      dihapus: false
+    },
+    {
+      nama: "Hangover - SALAZAR - Kaos Lengan Pendek Pria & Wanita",
+      harga: 75000,
+      video: "RIDDLE.mp4",
+      link: "https://s.shopee.co.id/AUjMpz0nO3",
+      dihapus: false
+    },
+  ]
+  produkList = dataAwal
+  saveProdukList(produkList)
+}
+
+
+
 function loadProdukList() {
   const data = localStorage.getItem(STORAGE_KEY)
   return data ? JSON.parse(data) : []
@@ -84,10 +137,12 @@ function renderProduk() {
     card.className = "bg-white rounded-xl shadow p-4"
 
     card.innerHTML = `
-      <video class="w-full h-64 rounded-xl object-cover mb-2" autoplay muted loop playsinline>
-        <source src="video/${item.video}" type="video/mp4" />
-        Browser tidak mendukung video.
-      </video>
+      <a href="${item.link}" target="_blank" rel="noopener noreferrer">
+        <video class="w-full h-64 rounded-xl object-cover mb-2 cursor-pointer" autoplay muted loop playsinline>
+          <source src="video/${item.video}" type="video/mp4" />
+          Browser tidak mendukung video.
+        </video>
+      </a>
       <h3 class="text-lg font-bold text-gray-800">${item.nama}</h3>
       <p class="text-orange-600 font-semibold mb-3">Rp${item.harga.toLocaleString()}</p>
       <div class="flex gap-2">
@@ -98,6 +153,7 @@ function renderProduk() {
     container.appendChild(card)
   })
 }
+
 
 function hapusProdukHandler(indexAktif) {
   if (confirm("Yakin ingin menghapus produk ini?")) {
