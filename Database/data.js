3496a19507f8498e38e5f8bc789f73d51968b59a
Gandinya <muhammaddedegandi01@gmail.com>
2025-07-28 11:45:19 +0700

    Update data.js

--- Database/data.js
+++ Database/data.js
@@ -67,3 +67,54 @@ function getProdukAsliIndex(produkAktifIndex) {
 function getProdukByIndexAktif(i) {
   return getProduk()[i]
 }
+
+let produkList = loadProdukList()
+
+if (produkList.length === 0) {
+  const dataAwal = [
+    {
+      nama: "Celana Cargo Baggy Wanita",
+      harga: 146850,
+      video: "OUTFIT BAGGY.mp4",
+      link: "https://s.shopee.co.id/9zmy31XSGD",
+      dihapus: false
+    },
+    {
+      nama: "Workshirt Cutting Boxy Pria",
+      harga: 197500,
+      video: "Workshirt Boxy.mp4",
+      link: "https://s.shopee.co.id/4ApBKfHkUy",
+      dihapus: false
+    },
+    {
+      nama: "Kemeja Fit Body Wanita",
+      harga: 68400,
+      video: "KemejaCeweFitBody.mp4",
+      link: "https://s.shopee.co.id/8ALKJ88hGX",
+      dihapus: false
+    },
+    {
+      nama: "Hangover - WOLLARD - Kaos Lengan Pendek Pria & Wanita",
+      harga: 75000,
+      video: "Hangover - WOLLARD.mp4",
+      link: "https://s.shopee.co.id/8pb8qWE95N",
+      dihapus: false
+    },
+    {
+      nama: "Hangover - HGVR 2.0 - Kaos Lengan Pendek Pria & Wanita",
+      harga: 75000,
+      video: "HGVR 2.0.mp4",
+      link: "https://s.shopee.co.id/8KesFklkU5",
+      dihapus: false
+    },
+    {
+      nama: "Hangover - SALAZAR - Kaos Lengan Pendek Pria & Wanita",
+      harga: 75000,
+      video: "RIDDLE.mp4",
+      link: "https://s.shopee.co.id/AUjMpz0nO3",
+      dihapus: false
+    },
+  ]
+  produkList = dataAwal
+  saveProdukList(produkList)
+}
