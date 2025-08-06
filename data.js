a30b2fd0f8a04593d820397aeb2d4da89fb4b3af
Gandinya <muhammaddedegandi01@gmail.com>
2025-08-06 17:11:52 +0700

    Update data.js

--- data.js
+++ data.js
@@ -1,23 +1,16 @@
 const jerseyList = [
   {
-    nama: "Hangover - PLEASURE - T-Shirt",
-    harga: 75000,
-    video: "PLEASURE.mp4",
-    link: "https://s.shopee.co.id/8zuZCYRref",
+    nama: "Elshoot Global | Jersey | Pon Banten Away 2024",
+    harga: 200000,
+    video: "Pon Banten Away 2024.mp4",
+    link: "https://s.shopee.co.id/LcpR597ph",
     dihapus: false
   },
   {
-    nama: "Manchester United - Home 2025",
-    harga: 135000,
-    video: "MUHome2025.mp4",
-    link: "https://shopee.co.id/jersey-mu",
+    nama: "Elshoot Global | Jersey | Pon Banten Home 2024 Sponsorless",
+    harga: 190000,
+    video: "Pon Banten Home 2024 Sponsorless.mp4",
+    link: "https://s.shopee.co.id/4L8yCdZnWK",
     dihapus: false
   },
-  {
-    nama: "Real Madrid - Away 2025",
-    harga: 150000,
-    video: "RMadridAway.mp4",
-    link: "https://shopee.co.id/jersey-madrid",
-    dihapus: false
-  }
 ];
