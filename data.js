131d03a363027856fc48851a91d060bb76a1d1db
Gandinya <muhammaddedegandi01@gmail.com>
2025-08-06 15:55:46 +0700

    Create data.js

---
+++ data.js
@@ -0,0 +1,23 @@
+const jerseyList = [
+  {
+    nama: "Hangover - PLEASURE - T-Shirt",
+    harga: 75000,
+    video: "PLEASURE.mp4",
+    link: "https://s.shopee.co.id/8zuZCYRref",
+    dihapus: false
+  },
+  {
+    nama: "Manchester United - Home 2025",
+    harga: 135000,
+    video: "MUHome2025.mp4",
+    link: "https://shopee.co.id/jersey-mu",
+    dihapus: false
+  },
+  {
+    nama: "Real Madrid - Away 2025",
+    harga: 150000,
+    video: "RMadridAway.mp4",
+    link: "https://shopee.co.id/jersey-madrid",
+    dihapus: false
+  }
+];
